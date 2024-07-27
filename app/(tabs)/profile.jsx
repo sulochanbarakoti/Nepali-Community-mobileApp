ced75ee3f17ce3fb454c64001bcf0f3b6452b39a
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-07-27 08:58:57 +0300

    profile screen worked

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
@@ -9,10 +9,10 @@ import CustomButton from "../../components/customButton";
 const Profile = () => {
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
   return (
-    <SafeAreaView className="bg-gray-300 h-full">
+    <SafeAreaView className="bg-gray-200 h-full">
       <ScrollView>
-        <View className="h-full">
-          <View className="flex-row justify-between pl-2">
+        <View className="h-full space-y-5">
+          {/* <View className="flex-row justify-between pl-2">
             <AntDesign name="arrowleft" size={24} color="black" />
             <Text className="text-lg font-bold text-primary">Profile</Text>
             <MaterialCommunityIcons
@@ -20,9 +20,9 @@ const Profile = () => {
               size={24}
               color="black"
             />
-          </View>
-          <View className=" mt-4 w-[95%] justify-center items-center">
-            <View className=" w-full justify-center bg-white items-center rounded-2xl p-2">
+          </View> */}
+          <View className=" mt-4 justify-center items-center">
+            <View className=" w-[95%] h-[200px] justify-center bg-white items-center rounded-2xl space-y-2">
               <Image
                 source={{ uri: user?.avatar }}
                 className="w-[150px] h-[150px] rounded-full"
@@ -31,27 +31,66 @@ const Profile = () => {
               <Text className="text-xl font-bold">@{user.username}</Text>
             </View>
           </View>
-          <View className="w-[80%] h-[60px] bg-gray-300 mt-5 rounded-xl flex-row justify-start items-center space-x-2 pl-2">
-            <Ionicons name="document" size={35} color="black" />
-            <Text className="text-xl font-bold">Orders</Text>
+          <View>
+            <View>
+              <Text className="text-lg font-bold ml-2">Profile Setting</Text>
+            </View>
+            <View className="justify-center items-center">
+              <View className="bg-primary h-[180px] w-[95%] rounded-xl justify-center items-center space-y-2">
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput
+                    className="w-full"
+                    placeholder="First name"
+                    editable={false}
+                  />
+                </View>
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput
+                    className="w-full"
+                    placeholder="Last name"
+                    editable={false}
+                  />
+                </View>
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput
+                    className="w-full"
+                    placeholder="email@gmeil.com"
+                    editable={false}
+                  />
+                </View>
+              </View>
+            </View>
           </View>
-          <View className="w-[80%] h-[60px] bg-gray-300 mt-5 rounded-xl flex-row justify-start items-center space-x-2 pl-2">
-            <Ionicons name="document" size={35} color="black" />
-            <Text className="text-xl font-bold">Orders</Text>
+          <View>
+            <View>
+              <Text className="text-lg font-bold ml-2">Security</Text>
+            </View>
+            <View className="justify-center items-center">
+              <View className="bg-primary h-[130px] w-[95%] rounded-xl justify-center items-center space-y-2">
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput
+                    className="w-full"
+                    placeholder="old password"
+                    editable={false}
+                  />
+                </View>
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput
+                    className="w-full"
+                    placeholder="new password"
+                    editable={false}
+                  />
+                </View>
+              </View>
+            </View>
           </View>
-          <View className="w-[80%] h-[60px] bg-gray-300 mt-5 rounded-xl flex-row justify-start items-center space-x-2 pl-2">
-            <Ionicons name="document" size={35} color="black" />
-            <Text className="text-xl font-bold">Orders</Text>
+
+          <View className="justify-center items-center">
+            <CustomButton
+              title="Log Out"
+              otherStyle="bg-red-700 w-[40%] mb-3"
+            />
           </View>
-          <View className="w-[80%] h-[60px] bg-gray-300 mt-5 rounded-xl flex-row justify-start items-center space-x-2 pl-2">
-            <Ionicons name="document" size={35} color="black" />
-            <Text className="text-xl font-bold">Orders</Text>
-          </View>
-          <View className="w-[80%] h-[60px] bg-gray-300 mt-5 rounded-xl flex-row justify-start items-center space-x-2 pl-2">
-            <Ionicons name="document" size={35} color="black" />
-            <Text className="text-xl font-bold">Orders</Text>
-          </View>
-          <CustomButton title="Log Out" />
         </View>
       </ScrollView>
     </SafeAreaView>
